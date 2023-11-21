655a38d3cf88777aa0750a594d0d5afd57d767b9
llariola00 <llcariola@usep.edu.ph>
2023-11-21 19:11:26 +0800

    Started coding the attendance charts.

---
+++ assets/charts/attendance.js
@@ -0,0 +1,38 @@
+export function attendanceChart(chartData, chartLabels) {
+    const chartCanvas = document.getElementById("main-chart");
+    console.log("CANVAS_ELEMENT: ", chartCanvas);
+
+    const attendanceChart = new Chart(chartCanvas, {
+        type: "bar",
+        data: {
+            labels: chartLabels,
+            datasets: [
+                {
+                    label: "Attendance",
+                    backgroundColor: "rgba(54, 162, 235, 0.6)",
+                    borderWidth: 0,
+                    borderColor: "rgba(54, 162, 235, 1)",
+                    hoverBackgroundColor: "rgba(54, 162, 235, 1)",
+                    hoverBorderWidth: 3,
+                    borderRadius: 15,
+                    data: chartData,
+                },
+            ],
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+
+            layout: {
+                padding: 20,
+            },
+        },
+    });
+
+    attendanceChart.update();
+}
--- dateSelector.js
+++ dateSelector.js
@@ -1,11 +1,32 @@
+import { attendanceChart } from "./assets/charts/attendance.js";
+
 export function setupDateSelectorEventListeners() {
     const selectedYear = document.getElementById("selectedYear");
     const selectedMonth = document.getElementById("selectedMonth");
-
     const year = document.getElementById("card-year");
     const month = document.getElementById("card-month");
 
-    function test_setData() {
+    const chartData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    const chartLabels = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ];
+
+    function setAttendanceChart() {
+        attendanceChart(chartData, chartLabels);
+    }
+
+    function setCardDate() {
         year.innerHTML = selectedYear.value;
         month.innerHTML = selectedMonth.value;
     }
@@ -13,13 +34,15 @@ export function setupDateSelectorEventListeners() {
     function setTitle() {}
 
     if (selectedMonth) {
-        selectedMonth.addEventListener("change", test_setData);
+        selectedMonth.addEventListener("change", setCardDate);
     }
 
     if (selectedYear) {
-        selectedYear.addEventListener("change", test_setData);
+        selectedYear.addEventListener("change", setCardDate);
+        selectedYear.addEventListener("change", setAttendanceChart);
     }
 
     // Call test_setData to set initial values
-    test_setData();
+    setCardDate();
+    setAttendanceChart();
 }
